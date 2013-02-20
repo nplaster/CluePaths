a44e21db97469fc50318d00a24b3b1067216a94f
Naomi Plasterer <nplaster@mymail.mines.edu>
2013-02-20 12:20:26 -0700

    Initial commit of my files

---
+++ IntBoard.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+
+//Naomi Plasterer and Brandon Bosso
+
+public class IntBoard {
+	HashSet target = new HashSet();
+	public IntBoard() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void calcAdjacencies(){
+		
+	}
+	
+	//Talk to Brandon
+	public void startTargets(/*We need to decided what we want to code*/){
+		
+	}
+	
+	public HashSet getTargets(){
+		
+		//return a HashSet with no items
+		return target;	
+	}
+	
+	public LinkedList<Integer> getAdjList(){
+		
+		//returns the adjacency list for one cell
+		return null;
+	}
+	
+	public Integer calcIndex(int row, int column){
+		
+		//returns index
+		return null;
+	}
+}
