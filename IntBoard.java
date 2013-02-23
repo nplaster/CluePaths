8b13ce67d4cb121aa64a97890cb6d9dc6043341f
Brandon Bosso <brbosso@mines.edu>
2013-02-23 12:24:40 -0700

    calcAdjacencies is actually done this time, it should do every grid

--- IntBoard.java
+++ IntBoard.java
@@ -13,27 +13,30 @@ public class IntBoard {
 		Arrays.fill(visited, 0, 14 false);
 	}
 	
-	//We should find a better way to do this
-	public void calcAdjacencies(int row, int column){
-		LinkedList<Integer> adjs = new LinkedList<Integer>();
-		if(row + 1 > 3)
-			adjs.add(calcIndex(row-1,column));
-		else if(row-1 < 0)
-			adjs.add(calcIndex(row+1,column));
-		else {
-			adjs.add(calcIndex(row+1,column));
-			adjs.add(calcIndex(row-1,column));
+	public void calcAdjacencies(){
+		for(int row = 0; row <= 3; row++) {
+			for(int column = 0; column <= 3; column++) {
+				LinkedList<Integer> adjs = new LinkedList<Integer>();
+				if(row == 3)
+					adjs.add(calcIndex(row-1,column));
+				else if(row == 0)
+					adjs.add(calcIndex(row+1,column));
+				else {
+					adjs.add(calcIndex(row+1,column));
+					adjs.add(calcIndex(row-1,column));
+				}
+
+				if(column == 3)
+					adjs.add(calcIndex(row,column-1));
+				else if(column-1 == 0)
+					adjs.add(calcIndex(row,column+1));
+				else {
+					adjs.add(calcIndex(row,column+1));
+					adjs.add(calcIndex(row,column-1));
+				}
+				adjMtx.put(calcIndex(row,column),adjs);
+			}
 		}
-		
-		if(column + 1 > 3)
-			adjs.add(calcIndex(row,column-1));
-		else if(column-1 < 0)
-			adjs.add(calcIndex(row,column+1));
-		else {
-			adjs.add(calcIndex(row,column+1));
-			adjs.add(calcIndex(row,column-1));
-		}
-		adjMtx.put(calcIndex(row,column),adjs);
 	}
 	
 	public void startTargets(int location, int steps){
